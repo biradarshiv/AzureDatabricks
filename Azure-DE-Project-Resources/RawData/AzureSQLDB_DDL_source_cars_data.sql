7ec3addc967c76205fabae61760a1fb11f71504a
Shivakumar Biradar <39446993+biradarshiv@users.noreply.github.com>
2025-10-07 21:42:47 +0530

    Add files via upload

---
+++ Azure-DE-Project-Resources/RawData/AzureSQLDB_DDL_source_cars_data.sql
@@ -0,0 +1,19 @@
+create table source_cars_data
+(
+Branch_ID varchar(200),
+Dealer_ID varchar(200),
+Model_ID varchar(200),
+Revenue bigint,
+Units_Sold bigint,
+Date_ID  varchar(200),
+Day int,
+Month int,
+Year int,
+BranchName varchar(200),
+DealerName varchar(200),
+Product_Name varchar(200)
+);
+
+
+select * from source_cars_data;
+
